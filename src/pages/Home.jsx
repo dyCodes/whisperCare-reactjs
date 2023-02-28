a9a2128537648f83b49f786c7e38905b8d40477d
dyCodes <yusufdaudu51@gmail.com>
2023-02-28 04:01:25 +0100

    feat: Add qr/barcode scanner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Button, Card, Container, Link, Typography } from "@mui/material";
+import { Box, Button, Card, CircularProgress, Container, Dialog, Link, Typography } from "@mui/material";
 import Layout from "../components/Layout";
 import RecentQRScans from "../components/RecentQRScans";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import QRcode from "../assets/img/qrcode.svg";
+import Html5QrcodePlugin from "../components/Html5QrcodePlugin";
 
 const Home = () => {
+	const [openModal, setOpenModal] = useState(false);
+
 	return (
 		<Layout page="Home">
 			<Container maxWidth="sm" className="container">
 				<div className="header_card">
-					<Button className="scan_qrcode">
+					<Button className="scan_qrcode" onClick={() => setOpenModal(true)}>
 						<Card className="card">
 							<img src={QRcode} style={{ display: "block" }} alt="Scan QRcode" />
 							<Typography component="p" align="center" sx={{ fontWeight: 500 }}>
@@ -20,6 +23,8 @@ const Home = () => {
 						</Card>
 					</Button>
 				</div>
+
+				{openModal && <ScanQRcode setOpenModal={setOpenModal} openModal={openModal} />}
 
 				<Card className="start_review_card">
 					<Typography variant="h6" mb={2.5} className="heading">
@@ -39,6 +44,38 @@ const Home = () => {
 				<RecentQRScans />
 			</Container>
 		</Layout>
+	);
+};
+
+const ScanQRcode = ({ setOpenModal, openModal }) => {
+	const handleClose = () => {
+		setOpenModal(false);
+	};
+
+	const onNewScanResult = (decodedText, decodedResult) => {
+		console.log("decodedText", decodedText);
+		console.log("decodedResult", decodedResult);
+		setOpenModal(false);
+	};
+
+	return (
+		<Dialog fullWidth onClose={handleClose} open={openModal}>
+			<div style={{ padding: "26px 12px" }}>
+				<Html5QrcodePlugin
+					fps={10}
+					qrbox={{ width: 250, height: 480 }}
+					disableFlip={false}
+					qrCodeSuccessCallback={onNewScanResult}
+				/>
+
+				<Box sx={{ display: "flex", justifyContent: "center", mt: "20px", mb: "10px" }}>
+					<CircularProgress />
+				</Box>
+				<Typography component="h6" align="center" sx={{ fontWeight: 500 }}>
+					Scanning...
+				</Typography>
+			</div>
+		</Dialog>
 	);
 };
 
