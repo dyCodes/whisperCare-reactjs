e872256abeee3a74dd31231d1127b4e1b55f8bd2
dyCodes <yusufdaudu51@gmail.com>
2023-02-28 02:16:02 +0100

    feat: Implement Dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./main.css";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
@@ -7,33 +7,6 @@ import Review from "./pages/Review";
 import Settings from "./pages/Settings";
 
 function App() {
-	const theme = createTheme({
-		palette: {
-			mode: "light",
-			primary: {
-				main: "#9747ff",
-			},
-			secondary: {
-				main: "#c46fc6",
-			},
-		},
-		typography: {
-			fontFamily: "Poppins, sans-serif",
-		},
-		components: {
-			MuiCssBaseline: {
-				styleOverrides: `
-					@font-face {
-						font-family: 'Poppins, sans-serif';
-						font-style: normal;
-						font-display: swap;
-						font-weight: 400;
-					}
-				`,
-			},
-		},
-	});
-
 	const router = createBrowserRouter([
 		{
 			path: "/",
@@ -51,11 +24,10 @@ function App() {
 	]);
 
 	return (
-		<ThemeProvider theme={theme}>
+		<>
 			<CssBaseline />
-
 			<RouterProvider router={router} />
-		</ThemeProvider>
+		</>
 	);
 }
 
